491471e502e264dbe2bf1696a824623b59a07c54
SlumpG <shlomo.gangul1@gmail.com>
2024-06-04 03:50:33 +0300

    trying to refactor the ugly code before moving to the deploy script

--- src/DillemaGame.sol
+++ src/DillemaGame.sol
@@ -10,8 +10,9 @@ contract DillemaGame {
     uint256 public player1Choice;
     uint256 public player2Choice;
 
-    uint256 public player1Deposit;
-    uint256 public player2Deposit;
+    // uint256 public player1Deposit;
+    // uint256 public player2Deposit;
+    ERC20 public tokenPool;
 
     uint256 public player1Points;
     uint256 public player2Points;
@@ -42,48 +43,45 @@ contract DillemaGame {
         gameCount++;
     }
 
-    function getGameCount() external view returns (uint256) {
-        return gameCount;
+    function withdrawOnJoinGame(address _player) external {
+        require(_player == address(0), "Address does not exist");
+        token.transfer(_player, tokenAmount / 2);
     }
 
-    function getIsGameOver() external view returns (bool) {
-        return isGameOver;
-    }
+    // function joinGamePlayer1(address _player1) external {
+    //     require(player1 != address(0), "Game has no player 1");
+    //     require(player2 == address(0), "Game already has two players");
+    //     player1 = _player1;
+    //     require(player1 != address(0), "Player 1 faild to join Game");
+    // }
 
-    function joinGamePlayer1(address _player1) external {
-        require(player1 != address(0), "Game has no player 1");
-        require(player2 == address(0), "Game already has two players");
-        player1 = _player1;
-        require(player1 != address(0), "Player 1 faild to join Game");
-    }
+    // function joinGamePlayer2(address _player2) external {
+    //     require(player1 != address(0), "Game has no player 1");
+    //     player2 = _player2;
+    //     require(player2 != address(0), "Player 2 faild to join Game");
+    // }
 
-    function joinGamePlayer2(address _player2) external {
-        require(player1 != address(0), "Game has no player 1");
-        player2 = _player2;
-        require(player2 != address(0), "Player 2 faild to join Game");
-    }
+    // function depositPlayer1(
+    //     address _player1,
+    //     ERC20 _token,
+    //     uint256 _amount
+    // ) external {
+    //     require(player1 == _player1, "Player 1 is not the sender");
+    //     require(_amount != 0, "Can't deposit 0 tokens");
+    //     require(_token == token, "Token is not the same as the game token");
+    //     player1Deposit = _amount;
+    // }
 
-    function depositPlayer1(
-        address _player1,
-        ERC20 _token,
-        uint256 _amount
-    ) external {
-        require(player1 == _player1, "Player 1 is not the sender");
-        require(_amount != 0, "Can't deposit 0 tokens");
-        require(_token == token, "Token is not the same as the game token");
-        player1Deposit = _amount;
-    }
-
-    function depositPlayer2(
-        address _player2,
-        ERC20 _token,
-        uint256 _amount
-    ) external {
-        require(player2 == _player2, "Player 2 is not the sender");
-        require(_amount != 0, "Can't deposit 0 tokens");
-        require(_token == token, "Token is not the same as the game token");
-        player2Deposit = _amount;
-    }
+    // function depositPlayer2(
+    //     address _player2,
+    //     ERC20 _token,
+    //     uint256 _amount
+    // ) external {
+    //     require(player2 == _player2, "Player 2 is not the sender");
+    //     require(_amount != 0, "Can't deposit 0 tokens");
+    //     require(_token == token, "Token is not the same as the game token");
+    //     player2Deposit = _amount;
+    // }
 
     function setPlayer1Choice(address _player1, uint256 _choice) external {
         require(player1 == _player1, "Player 1 is not the sender");
@@ -123,6 +121,14 @@ contract DillemaGame {
         player2Choice = 0;
         player1ChoiceCommitted = false;
         player2ChoiceCommitted = false;
+    }
+
+    function getGameCount() external view returns (uint256) {
+        return gameCount;
+    }
+
+    function getIsGameOver() external view returns (bool) {
+        return isGameOver;
     }
 
     function getWinner() external returns (address) {
